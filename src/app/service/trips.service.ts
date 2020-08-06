17eb171e3ab27aca4ba7d60eb96c1e76a4d93f95
wankhede04 <wankhedevijay04@gmail.com>
2020-08-06 09:28:11 +0530

    Added trip service for API calls.

---
+++ src/app/service/trips.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpService, IHttpResponse } from './http.service';
+import { Store } from '@ngxs/store';
+import { Observable, of } from 'rxjs';
+import { ITripsDetails } from '../store/trips/trips.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripsService {
+
+  constructor(private httpService: HttpService, private store: Store) { }
+
+  public getTripsDetails(): Observable<IHttpResponse> {
+    return this.httpService.GET(`${environment.serverUrl}${environment.apiPrefix}`);
+  }
+
+  public createTrip(payload: ITripsDetails): Observable<IHttpResponse> {
+    return this.httpService.POST(`${environment.serverUrl}${environment.apiPrefix}${payload.id}`, payload);
+  }
+
+  public updateTrip(payload: ITripsDetails): Observable<IHttpResponse> {
+    return this.httpService.PUT(`${environment.serverUrl}${environment.apiPrefix}${payload.id}`, payload);
+  }
+
+  public deleteTrip(id: number): Observable<IHttpResponse> {
+    return this.httpService.DELETE(`${environment.serverUrl}${environment.apiPrefix}${id}`);
+  }
+}
