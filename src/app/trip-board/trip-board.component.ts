c98efe15d4aed5b4be9e81f32183934a213a2374
wankhede04 <wankhedevijay04@gmail.com>
2020-08-06 19:28:44 +0530

    Implemented ngb-datepicker for showing the calendar view.

--- src/app/trip-board/trip-board.component.ts
+++ src/app/trip-board/trip-board.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, Input, SimpleChange } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { isEmpty } from 'lodash';
+import { Component, OnInit, Output, EventEmitter, Input, SimpleChange, ViewChild, ElementRef } from '@angular/core';
 import { ITripsDetails } from '../store/trips/trips.model';
+import {NgbDateStruct, NgbCalendar, NgbDatepicker} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-trip-board',
@@ -9,6 +8,7 @@ import { ITripsDetails } from '../store/trips/trips.model';
   styleUrls: ['./trip-board.component.scss']
 })
 export class TripBoardComponent implements OnInit {
+  @ViewChild('dp') calendarDate: NgbDatepicker;
 
   @Output()
   public tripDetailsEvent: EventEmitter<string> = new EventEmitter();
@@ -19,27 +19,31 @@ export class TripBoardComponent implements OnInit {
   @Input()
   public currentTripID: number;
 
-  private _selectedDate: BehaviorSubject<string> = new BehaviorSubject('');
+  model: NgbDateStruct;
+  date: {year: number, month: number};
+  public currentTrip: any;
 
-  public get selectedDate(): string {
-    return this._selectedDate.getValue();
+  constructor(private calendar: NgbCalendar) {
+    this.model = this.calendar.getToday();
   }
 
-  public set selectedDate(inputValue: string) {
-    this._selectedDate.next(inputValue);
+  ngOnInit() {}
+
+  ngOnChanges(changes?: SimpleChange) {
+    this.currentTrip = this.allTrips.filter(trip => trip.id === this.currentTripID);
+    if(this.currentTrip.length) {
+      const date = new Date(this.currentTrip[0].start);
+      this.model = {
+        'day': date.getDate(),
+        'month': date.getMonth() + 1,
+        'year': date.getFullYear()
+      }
+      this.calendarDate.navigateTo(this.model);
+    }
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  public tripValue(event: string) {
+    const selectedDate = new Date(`${this.date.year} ${this.date.month} ${event}`).toISOString();
+    this.tripDetailsEvent.emit(selectedDate);
   }
-
-  ngOnChanges(changes: SimpleChange) {
-    let currentTrip = this.allTrips.filter(trip => trip.id === this.currentTripID);
-  }
-
-  public tripValue() {
-    this.tripDetailsEvent.emit(this.selectedDate);
-  }
-
 }
