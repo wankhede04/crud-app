9b0f253fd69a84b5be5c0642ec061e95d1d9105b
wankhede04 <wankhedevijay04@gmail.com>
2020-08-06 19:28:09 +0530

    Implemented dynamic functionality and data update at cards.

--- src/app/trip-metadata/trip-metadata.component.ts
+++ src/app/trip-metadata/trip-metadata.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ITripsDetails } from '../store/trips/trips.model';
+import { Trips } from '../store/trips/trips.actions';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-trip-metadata',
@@ -14,12 +16,34 @@ export class TripMetadataComponent implements OnInit {
   @Output()
   public selectedTripEvent: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  public destination: string;
+  public comment: string;
+
+  constructor(private store: Store) { }
 
   ngOnInit() {
+    this.destination = this.trips.destination;
+    this.comment = this.trips.comments;
   }
 
   public openTrip() {
     this.selectedTripEvent.emit(this.trips.id);
+  }
+
+  public deleteTrip(tripID: number) {
+    this.store.dispatch(new Trips.Delete(tripID));
+  }
+
+  public updateTripdetails(tripValue: string) {
+    console.log(this.destination, 'destination')
+    console.log(this.comment, 'comment')
+    const tripdetails: ITripsDetails = {
+      id: this.trips.id,
+      destination: this.destination,
+      start: this.trips.start,
+      duration: this.trips.duration,
+      comments: this.comment
+    }
+    this.store.dispatch(new Trips.Update(this.trips.id, tripdetails))
   }
 }
